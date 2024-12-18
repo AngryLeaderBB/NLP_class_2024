d1cd3f78289cecd8dab8fe3d6589864354c323ed
Bruno Berto <brunoberto2001@gmail.com>
2024-12-18 20:38:39 -0300

    Update activity 3 files

--- Atividade_3/find_best_hyperparameters.py
+++ Atividade_3/find_best_hyperparameters.py
@@ -1,99 +1,113 @@
 import pandas as pd
 import numpy as np
-
-from numpy import vectorize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from scipy.sparse import csr_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
 
 class BestParam:
-    def __init__(self, source_path:str, target_path:str=".") -> None:
+    def __init__(self, source_path: str = ".", target_path: str = ".") -> None:
         self.source_path = source_path
         self.target_path = target_path
 
     def __load_data(self):
+        """Load data from the source CSV in source path."""
         return pd.read_csv(self.source_path)
 
-    def split_data(self, seed: int):
-        # Read csv file on source path
-        data = self.__load_data()
+    def __vectorize_text(self, texts: pd.Series):
+        """Convert the text data into a sparse matrix using CountVectorizer."""
+        vectorizer = CountVectorizer()
+        return vectorizer.fit_transform(texts), vectorizer
 
-        # Split the texts from their respectives classes
+    def __save_data(self, data, filename: str):
+        """Save the data to the target path as a CSV file."""
+        pd.DataFrame(data.toarray() if hasattr(data, 'toarray') else data).to_csv(
+            self.target_path + f'/{filename}', index=False
+        )
+
+    def __split_and_save_data(self, texts, classes, seed: int=-1):
+        """Split the data into training and testing sets and save to CSV."""
+
+        # If seed is -1, use a random seed
+        if seed == -1:
+            seed = np.random.randint(0, 2**32 - 1)  # Generate a random seed
+        
+        # Split the data into train and test
+        text_train, text_test, class_train, class_test = train_test_split(
+            texts, classes, test_size=0.2, random_state=seed
+        )
+        
+        # Save the data to CSV
+        self.__save_data(text_train, 'text_train.csv')
+        self.__save_data(class_train, 'class_train.csv')
+        self.__save_data(text_test, 'text_test.csv')
+        self.__save_data(class_test, 'class_test.csv')
+
+
+    def prepare_data(self, seed: int):
+        """Load csv, vectorize, split, and save the data into target path."""
+        data = self.__load_data()
         texts = data["text"]
         classes = data["class"]
-        
-        # Transform the texts into a sparse table (word for each file)
-        vectorizer = CountVectorizer()
-        texts = vectorizer.fit_transform(texts)
 
-        # Split the train/test 80%/20%
-        text_train, text_test, class_train, class_test = train_test_split(texts, 
-                                          classes, test_size=0.2,random_state=seed)
+        # Vectorize the texts
+        texts_matrix, vectorizer = self.__vectorize_text(texts)
 
+        # Split and save the data
+        self.__split_and_save_data(texts_matrix, classes, seed)
 
-        # Save bag of words for training
-        pd.DataFrame(text_train.toarray(), columns=
-          vectorizer.get_feature_names_out()).to_csv(self.target_path + 
-                        '/text_train.csv', index=False)
-        
-        # Save labels for training
-        pd.DataFrame(class_train).to_csv(self.target_path + 
-                        '/class_train.csv', index=False)
-        
-        # Save bag of words for testing
-        pd.DataFrame(text_test.toarray(), columns=
-          vectorizer.get_feature_names_out()).to_csv(self.target_path + 
-                        '/text_test.csv', index=False)
-        
-        # Save labels for testing
-        pd.DataFrame(class_test).to_csv(self.target_path + 
-                        '/class_test.csv', index=False)
+    def __prepare_model_search(self):
+        """Prepare the models and their parameter grids for the grid search."""
+        return [
+            ('MultinomialNB', MultinomialNB(), 
+             {'alpha': np.logspace(-3, 3, 7), 'fit_prior': [True, False]}),
+            ('LogisticRegression', LogisticRegression(), 
+             {'C': [0.01, 0.1, 1, 10, 100], 'solver': ['lbfgs', 'liblinear', 'saga']}),
+            ('SVC', SVC(), {'C': [0.01, 0.1, 1, 10, 100], 'kernel': ['linear', 'rbf', 'poly']})
+        ]
 
-    def greedy_search(self, cv):
-        # Read train data
+    def __run_grid_search(self, model, param_grid, texts, classes, cv):
+        """Run grid search for a given model and parameters."""
+        grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=cv, n_jobs=-1, verbose=1)
+        grid_search.fit(texts, classes)
+        return grid_search
+
+    def __collect_grid_search_results(self, grid_search, model_name):
+        """Collect the results from the grid search."""
+        results = []
+        for params, mean_score in zip(grid_search.cv_results_["params"], grid_search.cv_results_["mean_test_score"]):
+            results.append({
+                "Model": model_name,
+                "Params": params,
+                "Mean Test Score": mean_score,
+            })
+        return results
+
+    def grid_search(self, cv):
+        """Perform grid search on multiple models and save the results on target path."""
+        # Load the training data
         texts = csr_matrix(pd.read_csv(self.target_path + '/text_train.csv'))
         classes = pd.read_csv(self.target_path + '/class_train.csv').squeeze()
 
-        # Models and parameters
-        models = [('MultinomialNB', MultinomialNB(), 
-                    {'alpha': np.logspace(-3, 3, 7),
-                    'fit_prior': [True, False]}),
+        # Prepare models and parameters
+        models = self.__prepare_model_search()
 
-                  ('LogisticRegression', LogisticRegression(), 
-                    {'C': [0.01, 0.1, 1, 10, 100],
-                    'solver': ['lbfgs', 'liblinear', 'saga']}),
-
-                  ('SVC', SVC(), {'C': [0.01, 0.1, 1, 10, 100],
-                   'kernel': ['linear', 'rbf', 'poly']})
-                ]
-
-        res = []
+        # Store results for all models
+        all_results = []
         for name, model, param_grid in models:
-            # Initiate grid search acording to model
-            grid_search = GridSearchCV(estimator=model, param_grid=param_grid,
-                                       cv=cv, n_jobs=-1, verbose=1)
+            # Run grid search
+            grid_search = self.__run_grid_search(model, param_grid, texts, classes, cv)
+            
+            # Collect results
+            model_results = self.__collect_grid_search_results(grid_search, name)
+            all_results.extend(model_results)
 
-            # Fit the data
-            grid_search.fit(texts, classes)
+        # Sort the results
+        all_results.sort(key=lambda x: (x["Model"], -x["Mean Test Score"]))
 
-            # Fill the results
-            for params, mean_score in zip(grid_search.cv_results_["params"],
-                                  grid_search.cv_results_["mean_test_score"]):
-              res.append({
-                "Model": name,
-                "Params": params,
-                "Mean Test Score": mean_score,
-              })
-
-        # Sort the results based on name then on test score
-        res.sort(key=lambda x: (x["Model"], -x["Mean Test Score"]))
-
-        # Save the Data Frame into a csv
-        pd.DataFrame(res).to_csv(self.target_path + 
-                        '/model_search_results.csv', index=False)
-    
+        # Save the results to a CSV file
+        pd.DataFrame(all_results).to_csv(self.target_path + '/model_search_results.csv', index=False)
